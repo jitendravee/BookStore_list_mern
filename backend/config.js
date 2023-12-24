b1552bb5b30868767c2905f84261841cb1ae24fc
jitendravee <jitendravee098@gmail.com>
2023-12-25 00:21:07 +0530

    key

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const MONGO_URL = "mongodb+srv://jitendra:Jitendra6323@cluster0.2iiqqvj.mongodb.net/books_collection?retryWrites=true&w=majority"
+export const MONGO_URL = "MONGO"
